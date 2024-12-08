3c61419b99e3c1de30abe2ef0fd11f6723bfc988
AngelLM <alm37454@gmail.com>
2024-12-08 13:48:04 +0100

    ROS2 innecesary directories removed

--- ws_thor/build/moveit_thor/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Thor-ROS/ws_thor/src/moveit_thor
-# Build directory: /Thor-ROS/ws_thor/build/moveit_thor
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ws_thor/build/urdf_thor/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/Thor-ROS/ws_thor/install/urdf_thor'
